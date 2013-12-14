0fabb2d3c74ef02168ba801989b26f4c1d4020b6
anonym <no@adress.net>
2013-12-14 23:28:45 +0100

    start function to discretize bezier curves

---
+++ ld28-gdx/src/uk/co/alynn/one/BezierRecursion.java
@@ -0,0 +1,11 @@
+package uk.co.alynn.one;
+
+public class BezierRecursion{
+
+    public static void bezierRecursion(int [] points , int iterations){
+    	String test = "iterations " + iterations + " points " + points[0];
+        System.out.println(test);
+ 
+ 
+    }
+} 
