559f63af3f1aadc588c9dd88231b36fa252bb2a5
anonym <no@adress.net>
2013-12-15 12:22:30 +0100

    finish first working example of bezier iteration
    still needs cleaning up

--- ld28-gdx-desktop/src/uk/co/alynn/one/Main.java
+++ ld28-gdx-desktop/src/uk/co/alynn/one/Main.java
@@ -2,6 +2,7 @@ package uk.co.alynn.one;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import java.lang.Math;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,8 +13,25 @@ public class Main {
 		cfg.height = 320;
 		
 		new LwjglApplication(new OneGame(), cfg);
-		int [] points = new int [14]; //number of points after iteration * 2 (x, y)
-		int iterations = 1;
-		BezierRecursion.bezierRecursion(points , iterations);
+		int iterations = 3;
+		int counter = (((int)Math.pow(2,iterations))*3) + 1;
+
+		// example of bezier iteration
+		// should be done for x- and y coordinates
+		String test = "Bezier:";
+		float [] xpoints = new float [counter]; //number of points after iteration
+		xpoints[0] = 0;
+		xpoints[1] = 1;
+		xpoints[2] = 2;
+		xpoints[3] = 3;
+
+		BezierIteration.bezierIteration(xpoints, iterations);
+		System.out.println(test);
+		// TESTING
+		while (counter > 0){
+			test = test + " and " + xpoints[counter - 1];
+			counter = counter - 1;
+		}
+        System.out.println(test);
 	}
 }
