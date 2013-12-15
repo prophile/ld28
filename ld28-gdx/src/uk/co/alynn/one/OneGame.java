b53124d52fbd9f1cdeddafe001aa395391d80ad2
Alastair Lynn <arplynn@gmail.com>
2013-12-15 10:39:49 +0000

    Maintain a texture manager with some things

--- ld28-gdx/src/uk/co/alynn/one/OneGame.java
+++ ld28-gdx/src/uk/co/alynn/one/OneGame.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import uk.co.alynn.one.render.RenderRequest;
+import uk.co.alynn.one.render.TextureManager;
 import uk.co.alynn.one.render.WorldRenderer;
 import uk.co.alynn.one.world.Segment;
 import uk.co.alynn.one.world.World;
@@ -13,19 +14,16 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class OneGame implements ApplicationListener {
     private OrthographicCamera camera;
     private SpriteBatch batch;
-    private Texture texture;
     private Sprite sprite;
     private Constants constants;
     private World world;
+    private TextureManager textureManager;
 
     @Override
     public void create() {
@@ -33,18 +31,15 @@ public class OneGame implements ApplicationListener {
                 "data/constants.txt").reader());
         constants = ldr.load();
 
+        textureManager = new TextureManager();
+
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
 
         camera = new OrthographicCamera(1, h / w);
         batch = new SpriteBatch();
 
-        texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-
-        TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-
-        sprite = new Sprite(region);
+        sprite = new Sprite(textureManager.getTexture("libgdx"));
         sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
         sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
         sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
@@ -63,7 +58,6 @@ public class OneGame implements ApplicationListener {
     @Override
     public void dispose() {
         batch.dispose();
-        texture.dispose();
     }
 
     @Override
@@ -75,8 +69,8 @@ public class OneGame implements ApplicationListener {
         batch.begin();
         sprite.draw(batch);
         WorldRenderer renderer = new WorldRenderer(world, new RenderRequest(
-                constants, batch, null));
-        renderer.renderWorld();
+                constants, batch, textureManager));
+        // renderer.renderWorld();
         batch.end();
 
         WorldUpdater up = new WorldUpdater(world, constants, 1 / 30.0);
