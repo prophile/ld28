9710b3a8c8a8a7c4c968e5f8c4a0a703b4259f90
anonym <no@adress.net>
2013-12-15 12:53:54 +0100

    stop using outdated data

--- ld28-gdx/src/uk/co/alynn/one/BezierIteration.java
+++ ld28-gdx/src/uk/co/alynn/one/BezierIteration.java
@@ -5,10 +5,8 @@ import java.lang.Math;
 public class BezierIteration{
 
     public static void bezierIteration(float [] points , int iterations){
-        int i = 0;      
-
+        int i = 0;
         while (i < iterations){
-
         	int step = ((int)Math.pow(2,i));
         	while (step > 0){
         		step = step - 1;
@@ -23,16 +21,16 @@ public class BezierIteration{
     // it: which iteration, counting up
     // points originally contains coordinates for four bezier points
     // these are replaced with the 7 points describing the replacing two bezier curves
-        int data = st*3; // where the data comes from
+        int data = st*3; // where the original data comes from
         int ofs = data*2; // where the new datapoints go
 
     	points[ofs + 6] = points[data + 3];							// final result point 6
-    	points[ofs + 5] = (points[data + 6] + points[data + 2])/2;  // final result point 5
+    	points[ofs + 5] = (points[ofs + 6] + points[data + 2])/2;  	// final result point 5
     	points[ofs + 3] = (points[data + 1] + points[data + 2])/2;  // intermediate result
     	points[ofs + 1] = (points[data + 0] + points[data + 1])/2;  // final result point 1
-    	points[ofs + 2] = (points[data + 1] + points[data + 3])/2;  // final result point 2
-    	points[ofs + 4] = (points[data + 3] + points[data + 5])/2;  // final result point 4
-    	points[ofs + 3] = (points[data + 2] + points[data + 4])/2;  // final result point 3
+    	points[ofs + 2] = (points[ofs + 1] + points[ofs + 3])/2;  	// final result point 2
+    	points[ofs + 4] = (points[ofs + 3] + points[ofs + 5])/2;  	// final result point 4
+    	points[ofs + 3] = (points[ofs + 2] + points[ofs + 4])/2;  	// final result point 3
     }
 } 
 
