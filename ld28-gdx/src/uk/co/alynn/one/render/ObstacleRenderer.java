0d64e1eca6aad7bd31835c1ad55d1d9094b0f667
Alastair Lynn <arplynn@gmail.com>
2013-12-16 19:11:38 +0000

    Render obstacles with colours

--- ld28-gdx/src/uk/co/alynn/one/render/ObstacleRenderer.java
+++ ld28-gdx/src/uk/co/alynn/one/render/ObstacleRenderer.java
@@ -6,13 +6,12 @@ import uk.co.alynn.one.world.Obstacle;
 import uk.co.alynn.one.world.Side;
 import uk.co.alynn.one.world.level.LevelUtil;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 
 final class ObstacleRenderer {
     public static void renderObstacles(WorldRenderer worldRenderer) {
-        Iterator<Obstacle> obstacles = worldRenderer.getWorld().obstaclesBetween(0.0,
-                1.0);
+        Iterator<Obstacle> obstacles = worldRenderer.getWorld()
+                .obstaclesBetween(0.0, 1.0);
         worldRenderer.setUnitTransform();
         while (obstacles.hasNext()) {
             Obstacle ob = obstacles.next();
@@ -22,8 +21,10 @@ final class ObstacleRenderer {
             Vector2 eyes = LevelUtil.position(worldRenderer.getWorld()
                     .getLevel(), ob.getPosition().getT(), ob.getPosition()
                     .getSide() == Side.SIDE_A ? 25.0 : -25.0);
-            worldRenderer.drawSprite("" + ob.getValue(), eyes.x, eyes.y,
-                    32.0f, Color.GREEN);
+            worldRenderer.drawSprite("" + ob.getValue(), eyes.x, eyes.y, 32.0f,
+                    ob.getValue() == 1 ? worldRenderer.getRequest()
+                            .getColourScheme().getForeground() : worldRenderer
+                            .getRequest().getColourScheme().getBad());
             // worldRenderer.getShapeRenderer().begin(ShapeType.Circle);
             // worldRenderer.getShapeRenderer().circle(eyes.x, eyes.y, 40.0f);
             // worldRenderer.getShapeRenderer().end();
