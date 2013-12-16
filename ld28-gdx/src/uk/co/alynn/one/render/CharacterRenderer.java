431e855c64a8af5e0fc7a6e42e6a0faec53a22d8
Alastair Lynn <arplynn@gmail.com>
2013-12-16 19:23:15 +0000

    Yeah colours

--- ld28-gdx/src/uk/co/alynn/one/render/CharacterRenderer.java
+++ ld28-gdx/src/uk/co/alynn/one/render/CharacterRenderer.java
@@ -1,13 +1,21 @@
 package uk.co.alynn.one.render;
 
-import com.badlogic.gdx.graphics.Color;
+import uk.co.alynn.one.ColourScheme;
+
 import com.badlogic.gdx.math.Vector2;
 
 final class CharacterRenderer {
     public static void renderCharacter(WorldRenderer worldRenderer) {
         worldRenderer.setUnitTransform();
         Vector2 pp = worldRenderer.playerPosition();
-        worldRenderer.drawSprite("Player_1_64x64", pp.x, pp.y, 60.0f,
-                Color.WHITE);
+        ColourScheme scheme = worldRenderer.getRequest().getColourScheme();
+        final float HACK_IN_OFF_X = -26.5f, HACK_IN_OFF_Y = -27f;
+        final float HACK_OUT_OFF_X = -28.0f, HACK_OUT_OFF_Y = -28.0f;
+        worldRenderer.drawSprite("Player_1_outside_128x128", pp.x
+                + HACK_OUT_OFF_X, pp.y + HACK_OUT_OFF_Y, 86.0f,
+                scheme.getTrack());
+        worldRenderer.drawSprite("Player_1_inside_128x128", pp.x
+                + HACK_IN_OFF_X, pp.y + HACK_IN_OFF_Y, 72.0f,
+                scheme.getForeground());
     }
 }
